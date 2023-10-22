9ddf83ac68218ab91772304bd7a222b31cd48236
Samandar1418 <nabiyevs967@gmail.com>
2023-10-22 21:53:31 +0500

    mfdsa,

---
+++ index.js
@@ -0,0 +1,92 @@
+// 2 masala
+// let totalage =0;
+// let middleage ;
+// let peopleone = {
+//         Name : "Samandar",
+//         Surname: "Nabiyev",
+//         Age : 17
+//      }
+// let peopletwo = {
+//         Name : "Rozali",
+//         Surname: "Yoldashev",
+//         Age : 16
+//      }
+// let peoplethree = {
+//         Name : "Yaxyobek",
+//         Surname: "Nomonov",
+//         Age : 17
+//      }
+// let peoplefour = {
+//         Name : "Shermuhammad",
+//         Surname: "Ibrohimov",
+//         Age : 17
+//      }
+
+// let people = [
+//    peopleone,peopletwo,peoplethree,peoplefour
+// ];
+
+// for (let i = 0; i < people.length; i++) {
+//            totalage+=people[i].Age;
+// }
+
+// middleage = totalage/people.length;
+
+// console.log (middleage); 
+// 3 topshiriq
+
+// 4 topshiriq
+// let a = prompt ("Son kiriting:")
+// let result;
+// function sum () {
+//     if (a%2==1) {
+//         result = a**3
+//     } else if (a%2==0){
+//         result = a**2
+//     }
+
+//     return result;
+   
+// }
+// localStorage.setItem('Natija', sum ());
+//   5 topshiriq
+// birinchisi
+// function Person (name){
+//     this.name = name;
+// }
+// Person.prototype.sayHello = function (){
+// console.log ('Assalaykumalomu' + this.name + '!');
+// }
+// let person1 = new Person ('Samandar');
+// person1.sayHello();
+// ikkinchisi
+// function Person (name,surname,age){
+//     this.name = name;
+//     this.surname = surname;
+//     this.age = age;
+// }
+// Person.prototype.sayHello = function (){
+// console.log ('Assalomu alaykum'  + this.name + '!');
+// console.log ('Familyam'  + this.surname + '!');
+// console.log ('Yoshim'  + this.age + '!');
+// }
+// let person1 = new Person ('  Samandar','  Nabiyev','  17');
+// person1.sayHello();
+// uchinchisi
+ 
+// function Registration(Login, Password) {
+//     this.Login = Login;
+//     this.Password = Password;
+// }    
+
+// Registration.prototype.sayHello = function() {
+//     console.log('Login: ' + this.Login);
+//     console.log('Password: ' + this.Password);
+// }
+
+// const registration1 = new Registration('samandar17', 'parolyoq');
+// registration1.sayHello();
+
+
+
+
